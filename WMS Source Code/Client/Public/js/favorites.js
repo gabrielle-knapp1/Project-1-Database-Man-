b415e8556ddfb3e2164f2146c23bb9c535a26b7d
Gabrielle Knapp <116968433+gabrielle-knapp1@users.noreply.github.com>
2023-11-26 13:52:41 -0500

    Add files via upload
    updated warehouse and favorites js files --> sorting ability isn't updated yet, this just updates the database

---
+++ WMS Source Code/Client/Public/js/favorites.js
@@ -0,0 +1,38 @@
+document.addEventListener('DOMContentLoaded', () => {
+    RefreshTable();
+});
+
+async function RefreshTable() {
+    try {
+        const response = await fetch('/api/favorites', {
+            method: 'GET',
+            headers: { 'Content-Type': 'application/json' }
+        });
+        if (!response.ok) {throw new Error('Network response was not ok');}
+        const data = await response.json();
+        console.log(data);
+        if (data.success) {
+            // Assume you have an array containing favorites data called "Favorites"
+            const favorites = data.Favorites;
+
+            // Get the table body element
+            const tableBody = document.querySelector('tbody');
+
+            // Populate the table with item data
+            favorites.forEach(item => {
+                const row = document.createElement('tr');
+                row.id = `itemEntry${item.favoriteID}`;
+
+                const descriptionCell = document.createElement('td');
+
+                row.appendChild(document.createElement('td')).textContent = item.favoriteID;
+                row.appendChild(document.createElement('td')).textContent = item.username;
+                row.appendChild(document.createElement('td')).textContent = item.itemID;
+                tableBody.appendChild(row);
+            });
+        }
+    } catch (error) {
+        console.error('Error fetching favorites data:', error);
+        alert('An error occurred while fetching favorites data');
+    }
+}
