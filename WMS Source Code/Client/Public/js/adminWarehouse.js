4d16108b6e8741ac830cedf45fbf3c4269cf8b09
Gabrielle Knapp <116968433+gabrielle-knapp1@users.noreply.github.com>
2023-11-26 14:02:23 -0500

    Add files via upload
    Updated the adminWarehouse so it automatically syncs with the warehouse data - still has ability to add/edit data

--- WMS Source Code/Client/Public/js/adminWarehouse.js
+++ WMS Source Code/Client/Public/js/adminWarehouse.js
@@ -6,6 +6,47 @@
  * I've also added the css for pop-ups to the global.css file.
  * You can see how I added pop-ups if you look at adminTransactionLog.html and adminTransactionLog.js.
  */
+document.addEventListener('DOMContentLoaded', () => {
+    RefreshTable();
+});
+
+async function RefreshTable() {
+    try {
+        const response = await fetch('/api/warehouse', {
+            method: 'GET',
+            headers: { 'Content-Type': 'application/json' }
+        });
+        if (!response.ok) {throw new Error('Network response was not ok');}
+        const data = await response.json();
+        console.log(data);
+        if (data.success) {
+            // Assume you have an array containing warehouse data called "Items"
+            const warehouse = data.Items;
+
+            // Get the table body element
+            const tableBody = document.querySelector('tbody');
+
+            // Populate the table with item data
+            warehouse.forEach(item => {
+                const row = document.createElement('tr');
+                row.id = `itemEntry${item.itemID}`;
+
+                const descriptionCell = document.createElement('td');
+
+                row.appendChild(document.createElement('td')).textContent = item.itemID;
+                row.appendChild(document.createElement('td')).textContent = item.type;
+                row.appendChild(document.createElement('td')).textContent = item.name;
+                row.appendChild(document.createElement('td')).textContent = item.providerID;
+                row.appendChild(document.createElement('td')).textContent = item.placeID;
+                row.appendChild(document.createElement('td')).textContent = item.pricePerUnit;
+                tableBody.appendChild(row);
+            });
+        }
+    } catch (error) {
+        console.error('Error fetching warehouse data:', error);
+        alert('An error occurred while fetching warehouse data');
+    }
+}
 
 function addItem() {
     // Add fake item data
