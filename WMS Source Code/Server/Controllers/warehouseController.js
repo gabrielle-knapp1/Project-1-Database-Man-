ad3e065b0fcdeb0313da6b6e25462c72f392b639
Gabrielle Knapp <116968433+gabrielle-knapp1@users.noreply.github.com>
2023-11-28 14:51:30 -0500

    Update warehouseController.js
    typo

--- WMS Source Code/Server/Controllers/warehouseController.js
+++ WMS Source Code/Server/Controllers/warehouseController.js
@@ -18,7 +18,7 @@ function RemoveFavorite(req, res) {
 
 async function GetWarehouse(req, res) {
     //select all the items from the warehouse table
-    const rows = await mysql.selectQuery("select itemID, Type, name, providerID, stockQuanity, placeID, pricePerUnit from vItems", []);
+    const rows = await mysql.selectQuery("select itemID, Type, name, providerID, stockQuantity, placeID, pricePerUnit from vItems", []);
     res.send({ success: true, items: rows });
 }
 
