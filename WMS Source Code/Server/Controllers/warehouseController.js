2d9a75ddb1a5600f155a76754b9aa32e1c0ffd65
Gabrielle Knapp <116968433+gabrielle-knapp1@users.noreply.github.com>
2023-11-28 14:42:27 -0500

    Update warehouseController.js
    async

--- WMS Source Code/Server/Controllers/warehouseController.js
+++ WMS Source Code/Server/Controllers/warehouseController.js
@@ -16,7 +16,7 @@ function RemoveFavorite(req, res) {
     res.send({success: true});
 }
 
-function GetWarehouse(req, res) {
+async function GetWarehouse(req, res) {
     //select all the items from the warehouse table
     const rows = await mysql.selectQuery("select itemID, Type, name, providerID, stockQuanity, placeID, pricePerUnit from vItems", []);
     res.send({ success: true, items: rows })
