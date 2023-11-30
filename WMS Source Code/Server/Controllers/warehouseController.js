ee598a315de567d5f766b37c3796d3cc2d90ac92
Gabrielle Knapp <116968433+gabrielle-knapp1@users.noreply.github.com>
2023-11-30 15:59:50 -0500

    Update warehouseController.js
    Updating the controller so that it just edits the name, quantity, and price

--- WMS Source Code/Server/Controllers/warehouseController.js
+++ WMS Source Code/Server/Controllers/warehouseController.js
@@ -26,38 +26,18 @@ function AddItem(req, res) {}
 
 function EditItem(req, res) {
     try {
-        const {newItemID, originalItemID, type, name, providerID, stockQuantity, placeID, pricePerUnit} = req.body;
+        const {itemID, name, stockQuantity, pricePerUnit} = req.body;
         let sql = "update vItems set ";
         let values = [];
         let changed = false;
-        if (newItemID !== '') {
-            sql += "itemID=?, ";
-            values.push(newItemID);
-            changed = true;
-        }
-        if (type !== '') {
-            sql += "type=?, ";
-            values.push(type);
-            changed = true;
-        }
         if (name !== '') {
             sql += "name=?, ";
             values.push(name);
             changed = true;
         }
-        if (providerID !== '') {
-            sql += "providerID=?, ";
-            values.push(providerID);
-            changed = true;
-        }
         if (stockQuantity !== '') {
             sql += "stockQuantity=?, ";
             values.push(stockQuantity);
-            changed = true;
-        }
-        if (placeID !== '') {
-            sql += "placeID=?, ";
-            values.push(placeID);
             changed = true;
         }
          if (pricePerUnit !== '') {
@@ -70,7 +50,6 @@ function EditItem(req, res) {
         sql = sql.slice(0, -2);
         sql += " where itemID=?";
         mysql.insertQuery(sql, values);
-        values.push(originalItemID);
         res.send({ success: true, message: "Item updated" });
     } catch {
         res.send({ success: false, message: "Unkown error" });
