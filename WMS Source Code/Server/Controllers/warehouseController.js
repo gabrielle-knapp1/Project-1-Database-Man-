89c74f3d4ca0a7af305557d73b61df8e60ca5fa9
Ethan Morton <ethieq909@gmail.com>
2023-12-03 23:30:57 -0500

    added comments to RemoveItem()

--- WMS Source Code/Server/Controllers/warehouseController.js
+++ WMS Source Code/Server/Controllers/warehouseController.js
@@ -64,6 +64,16 @@ function EditItem(req, res) {
 
 function RemoveItem(req, res) {
     //use the item in the req.body and delete it from the admin warehouse
+
+    /*WARNING!!!
+    
+    I think it would be a bad idea to delete the item from the table for multiple reasons.
+    If the items table is on delete restrict, it just won't let us delete the item if it's being referenced somewhere else.
+    We could change it to on delete cascade, but that would delete all the references to that item, which means it would delete transaction logs and stuff.
+
+    I think the best thing to do would be to simply set the stockQuantity to 0.
+    Then when you display the items in the warehouse, just don't display the ones that are out of stock, or give an out of stock warning for those.
+    */
 }
 
 module.exports = {
