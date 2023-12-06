c6a00b9228723f6f3055cc49637bf1f03dd74fad
Ethan Morton <ethieq909@gmail.com>
2023-12-06 14:32:33 -0500

    ignore

--- WMS Source Code/Server/server.js
+++ WMS Source Code/Server/server.js
@@ -33,7 +33,7 @@ app.use(bodyParser.urlencoded({
 //Client permissions
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*'); // Allow requests from any origin
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
     next();
 });
